c78652fec901877c651e417a5fec420b654645c7
naresh099 <nareshkumarbudha@gmail.com>
2024-02-21 15:45:08 +0545

    day2

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -4,8 +4,8 @@ const Hero = () => {
     return (
         <div className="container pt-8 relative pb-3px">
             <div className="grid xl:grid-cols xl:grid-rows-2 gap-8">
-                <div className="relative xl:col-span-2 xl:row-start-1 xl:row-end-[-1]">
-                    <img className="w-full h-full object-cover rounded-lg" src="/image/hero__1.webp" alt="Hero image 1" />
+                <div className="relative xl:col-span-2   xl:row-start-1 xl:row-end-[-1]">
+                    <img className="w-full h-full object-cover pt-5 rounded-lg" src="/image/hero__1.webp" alt="Hero image 1" />
                     <div className="absolute max-w-[470px] sm:ml-16 ml-8 top-[50%] -translate-y-[50%] sm:space-y-4">
                         <p className="text-2xl hidden sm:block">100% Original Dry Fruits</p>
                         <h2 className="text-2xl sm:text-4xl md:text-6xl font-bold">Dried Fruits Best Quality</h2>
